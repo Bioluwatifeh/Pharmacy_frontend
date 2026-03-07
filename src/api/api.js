e8a36653efd4a20937e93efe20b5125ef9ee7565
Bioluwatifeh <boluawe.akanbi@gmail.com>
2026-03-07 13:42:52 +0530

    backend url update

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://pharmacybackend-production-3e54.up.railway.app/api"
 });
 
 export default API;
